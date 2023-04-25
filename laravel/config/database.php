bb031b94f8d4bc4b1956e61935120c61d930743d
Joel donaire diaz <jododi@fp.insjoaquimmir.cat>
2023-04-25 17:35:21 +0200

    Progreso basico hasta el punto de crear frontend guia m07

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -92,6 +92,11 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+        'options' => extension_loaded('pdo_mysql') ? [
+            PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY', '/var/lib/mysql/client-key.pem'),
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+         ] : [],
+         
 
     ],
 
