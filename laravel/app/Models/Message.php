bda563dbe2f7c5c901f37e42614aeee0a3579bf3
Ivan Moscoso <moscosoi2002@gmail.com>
2023-04-26 18:46:32 +0200

    juntada jhoel y ivan

--- laravel/app/Models/Message.php
+++ laravel/app/Models/Message.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    protected $fillable = [
+        'name',
+        'hora',
+        'text',
+        'data',
+        'archivo'
+    ];
     public function user()
     {
         return $this->belongsTo(User::class, 'id_user');
