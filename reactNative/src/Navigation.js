4f50258e1b4f87db47741682490ea4f808138ef1
Ivan Moscoso <moscosoi2002@gmail.com>
2023-05-10 19:27:33 +0200

    iconos puestos

--- reactNative/src/Navigation.js
+++ reactNative/src/Navigation.js
@@ -1,27 +1,64 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
 
 //paginas
 import CreateRoute from "./CreateRoute";
 import RutasList from "./RutasList";
 import ShowRoute from "./ShowRoute";
+const HomeStackNavigator = createNativeStackNavigator();
+function MyStack() {
+    return (
+        <HomeStackNavigator.Navigator
+        initialRouteName="listarScreen">
+            <HomeStackNavigator.Screen
+                name="listarScreen"
+                component={RutasList}
+            />
+            <HomeStackNavigator.Screen
+                name="ShowRoute"
+                component={ShowRoute}
+            />
+        </HomeStackNavigator.Navigator>
+    )
+}
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs(){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name="listar" component={RutasList}/>
-            <Tab.Screen name="create" component={CreateRoute}/>
-            <Tab.Screen name="ShowRoute" component={ShowRoute}/>
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="listar"
+            screenOptions={{
+                tabBarActiveTintColor: 'skyblue',
+
+
+            }}>
+            <Tab.Screen name="listar" component={MyStack}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="format-list-bulleted" size={size} color={color} />)
+                }} />
+            <Tab.Screen name="create" component={CreateRoute}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="add-road" size={size} color={color} />)
+
+                }}
+            />
+            <Tab.Screen name="ShowRoute" component={ShowRoute} />
         </Tab.Navigator>
     );
 }
-export default function Navigation(){
-    return(
+export default function Navigation() {
+    return (
         <NavigationContainer>
-            <MyTabs/>
+            <MyTabs />
         </NavigationContainer>
     )
 }
