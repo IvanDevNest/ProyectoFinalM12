c5e397cd69cff4346c57fa830ae8c98328dfcfc9
joeldonaire <joelator29@gmail.com>
2023-05-11 00:41:32 +0200

    Arreglo desincripciones modeloo

--- laravel/app/Models/Inscription.php
+++ laravel/app/Models/Inscription.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 class Inscription extends Model
 {
     use HasFactory;
-    
+    public $incrementing = false;
+
     protected $fillable = [
         'author_id',
         'route_id'
