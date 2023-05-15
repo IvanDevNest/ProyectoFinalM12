ca7c02e7499a9d90fe1744085c2623553ffc2454
joeldonaire <joelator29@gmail.com>
2023-05-15 19:47:19 +0200

    spatie permission arreglado lo que no iba en clase

---
+++ laravel/config/backpack/permissionmanager.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    |
+    | Models used in the User, Role and Permission CRUDs.
+    |
+    */
+
+    'models' => [
+        'user'       => config('backpack.base.user_model_fqn', \App\Models\User::class),
+        'permission' => Backpack\PermissionManager\app\Models\Permission::class,
+        'role'       => Backpack\PermissionManager\app\Models\Role::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disallow the user interface for creating/updating permissions or roles.
+    |--------------------------------------------------------------------------
+    | Roles and permissions are used in code by their name
+    | - ex: $user->hasPermissionTo('edit articles');
+    |
+    | So after the developer has entered all permissions and roles, the administrator should either:
+    | - not have access to the panels
+    | or
+    | - creating and updating should be disabled
+    */
+
+    'allow_permission_create' => true,
+    'allow_permission_update' => false,
+    'allow_permission_delete' => false,
+    'allow_role_create'       => false,
+    'allow_role_update'       => true,
+    'allow_role_delete'       => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Multiple-guards functionality
+    |--------------------------------------------------------------------------
+    |
+    */
+    'multiple_guards' => false,
+
+];
