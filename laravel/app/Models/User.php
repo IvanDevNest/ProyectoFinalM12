29f48fd9b2bf7099d3b3940884a1213d95265762
joeldonaire <joelator29@gmail.com>
2023-05-15 20:26:16 +0200

    quitado id_role de user.php

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -1,42 +1,41 @@
-<?php
+    <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
-
-// use Spatie\Permission\Traits\HasRoles;
-
-class User extends Authenticatable
-{   
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+    // use Illuminate\Contracts\Auth\MustVerifyEmail;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
+    use Laravel\Sanctum\HasApiTokens;
+    use Spatie\Permission\Traits\HasRoles;
 
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
-    use CrudTrait;
-    
-    public $guard_name = 'web';
+    // use Spatie\Permission\Traits\HasRoles;
 
-    // use HasRoles;
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'lastname',
-        'secound_sourname',
-        'file_id',
-        'id_role',
-        'route_id',
-        'author_id'
-    ];
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    class User extends Authenticatable
+    {   
+        use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+
+        use HasApiTokens, HasFactory, Notifiable, HasRoles;
+        use CrudTrait;
+        
+        public $guard_name = 'web';
+
+        // use HasRoles;
+        protected $fillable = [
+            'name',
+            'email',
+            'password',
+            'lastname',
+            'secound_sourname',
+            'file_id',
+            'route_id',
+            'author_id'
+        ];
+        protected $hidden = [
+            'password',
+            'remember_token',
+        ];
 
     public function inscriptions()
     {
