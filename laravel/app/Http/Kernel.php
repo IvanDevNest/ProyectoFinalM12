be2d1039bac45e642e515c05ef8c50a814e8b237
Ivan2 <moscosoi2002@gmail.com>
2023-05-22 20:30:47 +0200

    siu

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -63,7 +63,8 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class . ':1000,1', // Configuración de throttling
+
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class ,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
