ff79558b8c9116a97b236d7e375d5a315ba8d258
joeldonaire <joelator29@gmail.com>
2023-05-23 22:34:13 +0200

    script echo creo qno funciona

---
+++ laravel/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Clonar el repositorio desde GitHub
+git clone https://github.com/Guilirex/ProyectoFinalM12.git
+
+# Navegar al directorio del proyecto clonado
+cd laravel
+
+# Instalar las dependencias de Composer
+composer install
+
+# Copiar el archivo .env
+cp .env.example .env
+
+# Generar la clave de la aplicación
+php artisan key:generate
+
+# Crear enlace simbólico entre la carpeta public/storage y la carpeta storage/app/public
+php artisan storage:link
+
+# Ejecutar migraciones 
+php artisan migrate
+
+# Ejecutar seeders
+./seed.sh
+
+#
+cd ../reactNative
+
+#
+npm install
+
+
